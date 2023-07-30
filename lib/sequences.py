badec6fb35834f1351bbd4c17e0f3025b625afdc
Tony <thunter1987@yahoo.com>
2023-07-30 00:55:51 -0500

    passes tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+fibonacci_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 def print_fibonacci(length):
-    pass
+    numbers = ([num for num in fibonacci_list[0:length]])
+    print(numbers)
